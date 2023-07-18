6c7552b63009a847ed9bcca6bf99474c9c90d70d
Mohamed Atef <atefhamosa@gmail.com>
2023-07-18 09:37:37 +0300

    Delete laravel code

--- app/controllers/api/v1/user_reports_controller.rb
+++ app/controllers/api/v1/user_reports_controller.rb
@@ -1,9 +1,4 @@
 class Api::V1::UserReportsController < Api::V1::ApiController
-  $report = Report::create(['user_id' => $request->user_id, 'reported_id' => $request->reported_id, 'report' => $request->report,  'reported_type' => $request->reported_type]);
-  if ($request->hasFile('img')) {
-      $path = cloudinary()->upload($request->file('img')->getRealPath())->getSecurePath();
-      $report->img = $path;
-  }
   def create
     report = @current_user.user_reports.build(reported_id: params[:reported_id], reported_type: params[:reported_type], body: params[:body])
     if params[:img]
